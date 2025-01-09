4c3a1b61af20e1ee63039f9c4344edbb8846ba63
Sidharth Kumar <sidharthk.samal@nettantra.net>
2025-01-09 10:12:22 +0530

    changes

--- common/signup.js
+++ common/signup.js
@@ -1,6 +1,7 @@
 api.onPageChange(() => {
     const signupButton = document.getElementById('signup-btn');
     if (signupButton) {
+      console.log('signup button found');
       signupButton.addEventListener('click', () => {
         Discourse.User.current().showSignup();
       });
