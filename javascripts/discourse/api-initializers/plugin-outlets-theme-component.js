32ca4a4cfde7e79dbcde3e471494d70384b1b997
Sidharth Kumar <sidharthk.samal@nettantra.net>
2025-01-09 11:53:30 +0530

    updated

--- javascripts/discourse/api-initializers/plugin-outlets-theme-component.js
+++ javascripts/discourse/api-initializers/plugin-outlets-theme-component.js
@@ -7,8 +7,12 @@ export default apiInitializer('0.11.1', (api) => {
     api.headerIcons.add("plugin-outlets-header-icon", PluginOutletsHeaderIcon);
   }
   // if user is loged in only then show this before-header-panel
-  if (currentUser) {
-    console.log("currentUser", currentUser);
-    
-  }
+  api.onPageChange(() => {
+    const signupButton = document.getElementById('signup-btn');
+    if (signupButton) {
+      signupButton.addEventListener('click', () => {
+        Discourse.User.current().showSignup();
+      });
+    }
+  });
 });
